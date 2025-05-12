6513b6351aec9fca637e193d36f8789ca90b82e1
rennoank <rennoank@gmail.com>
2025-05-12 19:53:01 -0300

    Atualização2

--- src/main/java/com/api/parking_control/dto/ParkingSpotDto.java
+++ src/main/java/com/api/parking_control/dto/ParkingSpotDto.java
@@ -1,4 +1,33 @@
 package com.api.parking_control.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class ParkingSpotDto {
+
+    @NotBlank
+    private String parkingSpotNumber;
+
+    @NotBlank
+    @Size(max = 7)
+    private String licensePlateCar;
+
+    @NotBlank
+    private String brandCar;
+
+    @NotBlank
+    private String modelCar;
+
+    @NotBlank
+    private String colorCar;
+
+    @NotBlank
+    private String responsibleName;
+
+    @NotBlank
+    private String apartment;
+
+    @NotBlank
+    private String Block;
+
 }
--- src/main/java/com/api/parking_control/services/ParkingSpotService.java
+++ src/main/java/com/api/parking_control/services/ParkingSpotService.java
@@ -1,4 +1,23 @@
 package com.api.parking_control.services;
 
+import com.api.parking_control.models.ParkingSpotModel;
+import com.api.parking_control.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
 }
